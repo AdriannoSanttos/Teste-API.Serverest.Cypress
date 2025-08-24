9d0892d8ca6ada9f9ba3f0d6fdfd420a7df5da69
Adriano Santtos <j.adriannosanttos@gmail.com>
2025-08-24 11:17:52 -0300

    Cypress config ajustado

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,14 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "https://serverest.dev",
-    specPattern: "cypress/e2e/**/*.cy.js", 
+    specPattern: "cypress/e2e/**/*.cy.js",
+    reporter: "mochawesome",
+    reporterOptions: {
+      reportDir: "cypress/reports",
+      overwrite: false,
+      html: true,
+      json: true
+    },
     setupNodeEvents(on, config) {
       
     },
