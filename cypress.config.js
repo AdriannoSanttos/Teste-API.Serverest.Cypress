b4019dc8acc84a75bfc2892368c3cd9eb78b1fbc
Adriano Santtos <j.adriannosanttos@gmail.com>
2025-08-24 10:57:42 -0300

    baseUrl alterada

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,11 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "https://serverest.dev", 
+    specPattern: "cypress/e2e/api/**/*.cy.js",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+    
     },
-    baseUrl: "http://localhost:3000/"
   },
 });
+
